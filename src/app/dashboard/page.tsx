93d8eb0ccb25001cbe3494701e9e83b3424ff41d
Ashish Baravaliya <vier.institute35@gmail.com>
2023-11-28 17:21:21 -0500

    feat: dashboard header added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,19 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../_hooks/useAuth";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "../_components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../_components/ui/dropdown-menu";
+import { Button } from "../_components/ui/button";
 
 export default function Dashboard() {
   const router = useRouter();
-  const { loading, user } = useAuth();
+  const { loading, user, signOut } = useAuth();
 
   useEffect(() => {
     if (!loading && !user) {
@@ -14,5 +23,39 @@ export default function Dashboard() {
     }
   }, [loading, router, user]);
 
-  return user ? <div className="flex h-screen">dashboard</div> : null;
+  return user ? (
+    <div className="flex h-screen">
+      <div className="flex justify-between fixed inset-0 items-center h-16 px-6 border-b border-border">
+        <div className="relative z-20 flex items-center text-lg font-medium gap-2">
+          <Image src="/logo.png" alt="Logo" width={24} height={24} />
+          LetterLinc
+        </div>
+        <div className="flex items-center cursor-pointer">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                className="relative h-8 w-8 rounded-full"
+                size="icon"
+              >
+                <Avatar className="h-9 w-9">
+                  <AvatarFallback>
+                    {user?.name ? user?.name[0] : ""}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56 mt-1" align="end" forceMount>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => signOut()}
+              >
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </div>
+  ) : null;
 }
