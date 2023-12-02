c45e3f4080de72b93fc47045ff529a953bec37a6
Ashish Baravaliya <vier.institute35@gmail.com>
2023-12-01 23:06:33 -0500

    feat: view resumes and upload added

---
+++ src/app/_components/upload-button.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useRef } from "react";
+import { useAuth } from "../_hooks/useAuth";
+import { Button } from "./ui/button";
+import { useToast } from "../_hooks/use-toast";
+import { db, storage } from "../_lib/firebase";
+import axios from "axios";
+
+interface UploadButtonProps {
+  submitLoading: boolean;
+  setSubmitLoading: (submitLoading: boolean) => void;
+  successCallback?: () => void;
+}
+
+export const UploadButton: React.FC<UploadButtonProps> = ({
+  setSubmitLoading,
+  submitLoading,
+  successCallback,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { user } = useAuth();
+  const { toast } = useToast();
+  const storageRef = storage.ref();
+
+  const uploadFile = async (file: File) => {
+    if (file) {
+      setSubmitLoading(true);
+      toast({
+        title: "Uploading file...",
+      });
+      const filename = user.email + "/" + (+new Date() + "_" + file.name);
+
+      const fileRef = storageRef.child(filename);
+      const uploadTask = fileRef.put(file);
+
+      uploadTask.then(async () => {
+        const url = await fileRef.getDownloadURL();
+        axios
+          .post(`/api/run`, {
+            url,
+            vectorSpace: filename + "vectorSpace",
+          })
+          .then(() => {
+            db.collection("resumes")
+              .add({
+                createdAt: new Date().toISOString(),
+                email: user.email,
+                filename: file.name,
+                filepath: filename,
+                vectorSpace: filename + "vectorSpace",
+              })
+              .then(() => {
+                successCallback?.();
+                toast({
+                  variant: "success",
+                  title: "The file has been uploaded!",
+                });
+              });
+          })
+          .catch((error: any) => {
+            toast({
+              variant: "destructive",
+              title: `Failed to upload ${file.name}: ${error}`,
+            });
+            console.error(`Failed to upload ${file.name}: ${error}`);
+          })
+          .finally(() => {
+            setSubmitLoading(false);
+          });
+      });
+    }
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        ref={inputRef}
+        className="hidden"
+        accept="application/pdf"
+        name="files"
+        id="files"
+        onChange={async (e) => {
+          if (e.target.files?.length) {
+            //@ts-ignore
+            if (e.target.files[0].size > 1000000) {
+              toast({
+                variant: "destructive",
+                title: "File too large: 1MB max",
+              });
+            } else {
+              await uploadFile(e.target.files[0]);
+            }
+          }
+        }}
+      />
+      <Button
+        variant="outline"
+        onClick={() => inputRef.current?.click()}
+        disabled={submitLoading}
+      >
+        Upload
+      </Button>
+    </>
+  );
+};
---
+++ src/app/_components/view-resume-dialog.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { Button } from "./ui/button";
+import { Dialog, DialogContent } from "./ui/dialog";
+import { db, storage } from "../_lib/firebase";
+import { useAuth } from "../_hooks/useAuth";
+import { ExternalLink } from "lucide-react";
+import { UploadButton } from "./upload-button";
+
+interface UploadButtonProps {
+  submitLoading: boolean;
+}
+
+interface ResumeProps {
+  id: string;
+  filename: string;
+  createdAt: string;
+  url: string;
+}
+
+export const ViewResumeDialog: React.FC<UploadButtonProps> = ({
+  submitLoading,
+}) => {
+  const { user } = useAuth();
+  const storageRef = storage.ref();
+  const [open, setOpen] = useState(false);
+  const [fetching, setFetching] = useState(true);
+  const [resumesData, setResumesData] = useState<ResumeProps[]>([]);
+
+  const fetchPdfFiles = async () => {
+    setFetching(true);
+    db.collection("resumes")
+      .where("email", "==", user.email)
+      .get()
+      .then((res) => {
+        let resumes: ResumeProps[] = [];
+        res.forEach(async (doc) => {
+          const itemRef = storageRef.child(doc.data().filepath);
+          let url = await itemRef.getDownloadURL();
+          resumes.push({
+            id: doc.id,
+            createdAt: doc.data().createdAt,
+            filename: doc.data().filename,
+            url: url,
+          });
+        });
+        setResumesData(resumes);
+        setFetching(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchPdfFiles();
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        disabled={submitLoading}
+        onClick={() => setOpen(true)}
+      >
+        View all
+      </Button>
+      <Dialog
+        open={open}
+        onOpenChange={(toggle) => {
+          setOpen(toggle);
+        }}
+      >
+        <DialogContent className="p-8 border w-[740px] min-h-[460px] max-w-[740px] border-border bg-background">
+          <h4 className="text-lg">Your Resumes</h4>
+          <div className="flex flex-wrap gap-4 min-h-[282px]">
+            {fetching ? (
+              <p>Loading...</p>
+            ) : resumesData.length ? (
+              resumesData.map((resume, index) => (
+                <div
+                  key={index}
+                  className="w-52 p-2 border border-border bg-white/5"
+                >
+                  <embed src={resume.url} width="100%" height="200px" />
+                  <p className="truncate my-2">{resume.filename}</p>
+                  <div className="flex justify-between items-center">
+                    <p className="text-xs text-white/80">
+                      {moment(resume.createdAt).fromNow()}
+                    </p>
+                    <Button
+                      className="p-0 w-6 h-6"
+                      onClick={() => {
+                        window.open(resume.url, "_blank");
+                      }}
+                    >
+                      <ExternalLink className="w-4 h-4" />
+                    </Button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No resumes</p>
+            )}
+          </div>
+          <div className="flex flex-1 justify-end mt-3">
+            <UploadButton
+              submitLoading={submitLoading}
+              setSubmitLoading={() => {}}
+              successCallback={fetchPdfFiles}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
