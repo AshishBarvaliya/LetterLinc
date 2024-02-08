f0563ed46b4e0f72ebda45cb00486746ec613d95
sumehar <60893806+sumehar@users.noreply.github.com>
2024-02-07 20:55:46 -0500

    Backend and database issue solved

--- src/app/_lib/makechain.ts
+++ src/app/_lib/makechain.ts
@@ -2,22 +2,22 @@ import { OpenAI } from "langchain/llms/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { CallbackManager } from "langchain/callbacks";
-
-const QA_PROMPT = `You are a helpful cover letter AI assistant. Use the following pieces of context/resume information and job description to write a cover letter. 
-NOTE: MAXIMUM LENGTH OF THE LETTER MUST NOT BE MORE THAN 150 WORDS.
-
+ 
+const QA_PROMPT = `You're a supportive AI cover letter assistant. Craft a cover letter using the provided resume details and job description.
+Keep it concise, not exceeding 150 words.
+ 
 {context}
-
+ 
 Job description: {question}
 Cover letter:`;
-
+ 
 export const makeChain = (
   vectorstore: PineconeStore,
   onTokenStream?: (token: string) => void
 ) => {
   const model = new OpenAI({
-    temperature: 1,
-    modelName: "gpt-3.5-turbo",
+    temperature: 0.5,
+    modelName: "gpt-4-0125-preview",
     streaming: Boolean(onTokenStream),
     callbackManager: onTokenStream
       ? CallbackManager.fromHandlers({
@@ -27,7 +27,7 @@ export const makeChain = (
         })
       : undefined,
   });
-
+ 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
     vectorstore.asRetriever(),
